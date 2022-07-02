dce703b1e371d136eb6ae9530f5adba59141ab08
BayerischeMotorenWerke <syaifulamri199@gmail.com>
2022-07-02 14:14:30 +0700

    sm6150-common: Nusantara Project
    Signed-off-by: BayerischeMotorenWerke <syaifulamri199@gmail.com>

--- sm6150.mk
+++ sm6150.mk
@@ -22,7 +22,7 @@ $(call inherit-product-if-exists, vendor/miuicamera/config.mk)
 # Overlays
 DEVICE_PACKAGE_OVERLAYS += \
     $(LOCAL_PATH)/overlay \
-    $(LOCAL_PATH)/overlay-lineage
+    $(LOCAL_PATH)/overlay-nad
 
 PRODUCT_ENFORCE_RRO_TARGETS := *
 
@@ -192,11 +192,9 @@ PRODUCT_COPY_FILES += \
 PRODUCT_PACKAGES += \
     android.hardware.biometrics.fingerprint@2.3-service.xiaomi_sm6150
 
-# FM
+# Remove unwanted packages
 PRODUCT_PACKAGES += \
-    FM2 \
-    libqcomfm_jni \
-    qcom.fmradio
+    RemovePackages
 
 # Framework detect
 PRODUCT_PACKAGES += \
